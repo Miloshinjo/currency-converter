7ba4ebecbcfbbf7866194b3ec9ef0bba5305ff2b
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-09-24 10:02:43 +0200

    Build Logo component

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 
 import Home from './screens/Home'
 
+// Building color variables that will be accessable throghout the app
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A'
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF'
 })
 
 export default () => <Home /> // you can not export a component, but you can export a function that returns a component

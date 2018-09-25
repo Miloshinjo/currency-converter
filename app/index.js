819e3cbc945a8b28278469406a725d5e9abb8112
milos.dzeletovic <milos.dzeletovic@consulteer.com>
2018-09-25 08:26:03 +0200

    Add Text Input components

--- app/index.js
+++ app/index.js
@@ -6,7 +6,10 @@ import Home from './screens/Home'
 // Building color variables that will be accessable throghout the app
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
-  $white: '#FFFFFF'
+  $white: '#FFFFFF',
+  $border: '#e2e2e2',
+  $inputText: '#797979',
+  $lightGray: '#f0f0f0'
 })
 
 export default () => <Home /> // you can not export a component, but you can export a function that returns a component
